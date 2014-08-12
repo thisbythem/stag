a3306e5c4807746a29cc785a265524f12b53ca75
James Wagner <james@thisbythem.com>
2014-08-11 23:46:27 -0700

    Move SSH class into utilities

--- autoload_stag.php
+++ autoload_stag.php
@@ -3,6 +3,7 @@
 function autoload_stag() {
   require_once 'vendor/ftp/src/Ftp.php';
   require_once 'command.php';
+  require_once 'utilities/ssh.php';
 
   $commands = glob(__DIR__ . '/commands/command.*.php');
   foreach ($commands as $cmd) {
